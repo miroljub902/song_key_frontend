bbeb52abfb59e723b073ab439333d63aa005653d
miroljub902 <miroljub902@gmail.com>
2019-09-24 09:53:15 +0200

    upload file vueDropzone added

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -3,121 +3,28 @@
     <div>
     <h1>Song Key Detection</h1>
 
-    <form @submit.prevent="sendFile" enctype="multipart/form-data">
-        <div v-if="message"
-            :class="`message ${error ? 'is-danger' : 'is-success'}`"
-        >
-            <div class="message-body">{{message}}</div>
-        </div>
-
-        <div class="field">
-            <div class="file is-boxed is-primary">
-                <label class="file-label">
-                    <input
-                        multiple
-                        type="file"
-                        ref="files"
-                        @change="selectFile"
-                        class="file-input"
-                    />
-                    <span class="file-cta">
-                        <span class="file-icon"><i class="fa fa-upload"></i></span>
-                        <span>Choose a file...</span>
-                    </span>
-                    <span v-if="file" class="file-name">{{file.name}}</span>
-                </label>
-            </div>
-        </div>
-
-        <div class="field">
-            <div v-for="(file, index) in files" :key="index" 
-                :class="`level ${file.invalidMessage && 'has-textdanger'}`"
-            >
-                        <h1>abc</h1>
-                <div class="level-left">
-                    <div class="level-item">
-                        {{file.name}}
-                        <span v-if="file.invalidMessage">&nbsp;- {{file.invalidMessage}}</span>
-                    </div>
-                </div>
-                <div class="level-right">
-                    <div class="level-item">
-                        <a @click.prevent="files.splice(index,1); uploadFiles.splice(index, 1)" class="delete"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="field">
-            <button class="button is-info">Send</button>
-        </div>
-    </form>
+    <vue-dropzone ref="files" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
     
     </div>    
 </template>
 
 <script>
-import axios from "axios";
-import _ from 'lodash';
-
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 export default {
-  name: "Upload",
-  data() {
-      return {
-          files: [],
-          uploadFiles: [],
-          message: "",
-          error: false
-      };
+  name: 'app',
+  components: {
+    vueDropzone: vue2Dropzone
   },
-  methods: {
-      selectFile() {
-          const files = this.$refs.files.files;
-          this.files = [
-              ...this.files,
-              ..._.map(files, file => ({
-                  name: file.name,
-                  size: file.size,
-                  type: file.type,
-                  invalidMessage: this.validdate(files)
-              }))
-          ]
-      },
-
-      validdate(file) {
-
-          const MAX_SIZE = 200000;
-          const allowedTypes = ["image/mp3", "image/wav"];
-
-          if (file.size > MAX_SIZE) {
-              return `Max Size: ${MAX_SIZE/1000} kb`;
-          }
-
-          if (!allowedTypes.includes(file.type)) {
-              return "Not an audio";
-          }
-
-          return "";
-      },
-
-      async sendFile() {
-          const formData = new FormData();
-          _.forEach(this.uploadFiles, file => {
-              if (this.validdate(file) == "") {
-                  formData.append('files', file);
-              }
-          });
-
-          try {
-              await axios.post('/upload', formData);
-              this.message = "File has been uploaded";
-              this.files = [];
-              this.uploadFiles = [];
-          } catch (err) {
-              this.message = err.response.data.error;
-              this.error = true;
-          }
-      },
+  data: function () {
+    return {
+      dropzoneOptions: {
+          url: 'http://127.0.0.1:5000/api/upload',
+          thumbnailWidth: 150,
+          maxFilesize: 0.5,
+          headers: { "My-Awesome-Header": "header value" }
+      }
+    }
   }
-};
+}
 </script>
